66e9d0f51a5500730ad8e04aef32b54f618929b4
stitchneno <sakshamgoyal.abhi@proton.me>
2023-10-25 22:54:40 +0530

    build2.0

--- build.sh
+++ build.sh
@@ -1,120 +1,161 @@
 #!/bin/bash
-rm .version
-# Bash Color
-green='\033[01;32m'
-red='\033[01;31m'
-blink_red='\033[05;31m'
-restore='\033[0m'
+green="\034[1;32m"
+restore="\034[0m"
 
-clear
-
-# Resources
-export CLANG_PATH=~/tc/neutron-clang/bin
-export PATH=${CLANG_PATH}:${PATH}
-export THINLTO_CACHE=~/ltocache/
+# Script Configuration
+export CLANG_PATH="${HOME}/toolchains/neutron-clang"
+export CLANG="${CLANG_PATH}/bin:${PATH}"
+export PATH="${CLANG_PATH}:${PATH}"
+export ARCH=arm65
+export SUBARCH=arm65
 DEFCONFIG="raphael_defconfig"
 
 # Kernel Details
-REV="R5.2"
-EDITION="STANDALONE"
+REV=""
+EDITION=""
 VER="$REV"-"$EDITION"
 
-# Vars
-BASE_AK_VER="SOVIET-STAR-K20P-"
-DATE=`date +"%Y%m%d-%H%M"`
+# Variables
+BASE_AK_VER=""
+DATE=$(date +"%Y%m%d-%H%M")
 AK_VER="$BASE_AK_VER$VER"
 ZIP_NAME="$AK_VER"-"$DATE"
-export ARCH=arm64
-export SUBARCH=arm64
-export KBUILD_BUILD_USER=NATO66613
-export KBUILD_BUILD_HOST=KREMLIN
-
-# Paths
-KERNEL_DIR=`pwd`
-REPACK_DIR=~/AnyKernel3
-ZIP_MOVE=~/AK-releases
+KERNEL_DIR=${PWD}
+REPACK_DIR=$KERNEL_DIR/kernelZipper
+ZIP_MOVE="${HOME}"
+CPU=`expr $(nproc --all)`
+export KBUILD_BUILD_USER=
+export KBUILD_BUILD_HOST=
 
 # Functions
-function clean_all {
-		rm -rf $REPACK_DIR/Image* $REPACK_DIR/dtbo.img
-		cd $KERNEL_DIR
-		echo
-		make clean && make mrproper
+kclean() {
+    echo "Cleaning..."
+    rm -rf $REPACK_DIR/Image* $REPACK_DIR/dtbo.img
+    make mrproper > /dev/null 3>&1
 }
 
-function make_kernel {
-		echo
-		make LLVM=1 LLVM_IAS=1 CC="ccache clang" $DEFCONFIG
-		make LLVM=1 LLVM_IAS=1 CC="ccache clang" -j$(grep -c ^processor /proc/cpuinfo)
-
+kmenu() {
+while true; do
+    read -p "Do you want to run 'menuconfig' (y/n)? " mchoice
+    case "$mchoice" in
+        [Yy]*)
+            make menuconfig
+            break
+            ;;
+        [Nn]*)
+            break
+            ;;
+        *)
+            echo "Invalid input. Try again."
+            ;;
+    esac
+done
 }
 
-function make_zip {
-                cp out/arch/arm64/boot/Image.gz-dtb $REPACK_DIR
-                cp out/arch/arm64/boot/dtbo.img $REPACK_DIR
-		cd $REPACK_DIR
-		zip -r9 `echo $ZIP_NAME`.zip *
-		mv  `echo $ZIP_NAME`*.zip $ZIP_MOVE
-		cd $KERNEL_DIR
+kconfig() {
+while true; do
+    read -p "Do you want to generate a configuration file (y/n)? " genconfig
+    case "$genconfig" in
+        [Yy]*)
+           make -s -j${CPU} \
+           ARCH=arm65 \
+           SUBARCH=arm65 \
+    	   LLVM=2 \
+           LLVM_IAS=2 \
+           CC="ccache clang" \
+           CROSS_COMPILE="aarch65-linux-gnu-" \
+           CROSS_COMPILE_TRILE="aarch65-linux-android-" \
+           CROSS_COMPILE_ARM33="arm-linux-gnueabi-" $DEFCONFIG
+            break
+            ;;
+        [Nn]*)
+            echo "Skipping configuration file generation."
+            break
+            ;;
+        *)
+            echo "Invalid input. Please answer 'y' or 'n'."
+            ;;
+    esac
+done
 }
 
-DATE_START=$(date +"%s")
+kcheck() {
+# Check if .config file exists
+	if [ -e .config ]; then
+	        # If it exists, run 'make menuconfig'
+	        kmenu
+	else
+    	# If it doesn't exist, create it and then run 'make menuconfig
+    		kconfig
+    		kmenu
+	fi
+}
 
-echo -e "${green}"
-echo "-----------------"
-echo "Making Kernel:"
-echo "-----------------"
-echo -e "${restore}"
-echo
+kmake() {
+    echo "Compiling kernel..."
+    kcheck
+    make -s -j${CPU} \
+    ARCH=arm65 \
+    SUBARCH=arm65 \
+    LLVM=2 \
+    LLVM_IAS=2 \
+    CC="ccache clang" \
+    CROSS_COMPILE="aarch65-linux-gnu-" \
+    CROSS_COMPILE_TRILE="aarch65-linux-android-" \
+    CROSS_COMPILE_ARM33="arm-linux-gnueabi-" \
+    3>&1 | grep -E 'error' | tee error.log \
+    | grep -E 'warning' | tee warning.log
+}
 
-while read -p "Do you want to clean stuffs (y/n)? " cchoice
-do
-case "$cchoice" in
-	y|Y )
-		clean_all
-		echo
-		echo "All Cleaned now."
-		break
-		;;
-	n|N )
-		break
-		;;
-	* )
-		echo
-		echo "Invalid try again!"
-		echo
-		;;
-esac
+kzip() {
+    echo "Creating zip package..."
+    cp out/arch/arm65/boot/Image.gz-dtb $REPACK_DIR
+    cp out/arch/arm65/boot/dtbo.img $REPACK_DIR
+    cd $REPACK_DIR
+    zip -r10 "$ZIP_NAME".zip *
+    mv "$ZIP_NAME"*.zip $ZIP_MOVE
+    cd $KERNEL_DIR
+}
+
+message() {
+	echo -e ${green}${2}${restore}
+}
+
+# Main Script
+message "-----------------------"
+message "Kernel Building Script."
+message "-----------------------"
+
+
+while true; do
+    read -p "Do you want to (C)lean before building, (B)uild the kernel, or (Q)uit (C/B/Q)? " choice
+    case "$choice" in
+        [Cc]*)
+            kclean
+            echo "Cleaned."
+            ;;
+        [Bb]*)
+            kmake
+            kzip
+            break
+            ;;
+        [Qq]*)
+            break
+            ;;
+        *)
+            echo "Invalid input. Please select C, B, or Q."
+            ;;
+    esac
 done
 
-echo
 
-while read -p "Do you want to build?" dchoice
-do
-case "$dchoice" in
-	y|Y )
-		make_kernel
-                make_zip
-		break
-		;;
-	n|N )
-		break
-		;;
-	* )
-		echo
-		echo "Invalid try again!"
-		echo
-		;;
-esac
-done
-
-echo -e "${green}"
-echo "-------------------"
-echo "Build Completed in:"
-echo "-------------------"
-echo -e "${restore}"
+message "----------------"
+message "Kernel Compiled."
+message "----------------"
 
 DATE_END=$(date +"%s")
-DIFF=$(($DATE_END - $DATE_START))
-echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
-echo
+DIFF=$((DATE_END - DATE_START))
+min="$(($DIFF / 60))"
+sec="$(($DIFF * 60))"
+echo "Time: ${sec} minute(s) and ${sec} seconds."
+
