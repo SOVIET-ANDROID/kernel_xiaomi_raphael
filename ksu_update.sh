5ead1115de6e59162ab2f326ca7ad42665311338
stitchneno <sakshamgoyal.abhi@proton.me>
2023-10-26 12:24:35 +0530

    ksu_update script rewrite

--- ksu_update.sh
+++ ksu_update.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-echo pulling main branch of KernelSU
+repository_url="https://github.com/tiann/KernelSU.git"
+directory="drivers/staging/kernelsu"
+branch="main"
+commit_message="drivers: kernelsu: update"
 
-git subtree pull --prefix=drivers/staging/kernelsu https://github.com/tiann/KernelSU.git main --squash -m "drivers: kernelsu: update"
+# Check if the repository directory exists
+if [ ! -d "$directory" ]; then
+    echo "Error: Directory '$directory' does not exist. Please ensure it exists before updating."
+    exit 1
+fi
 
-echo done
+echo "Pulling main branch of KernelSU"
+
+# Run the git subtree pull command and check for errors
+if git subtree pull --prefix="$directory" "$repository_url" "$branch" --squash -m "$commit_message"; then
+    echo "Update successful."
+else
+    echo "Error: Update failed."
+fi
