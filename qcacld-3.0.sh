72228140d27fd7dd004aaa0e9d1111e4868e214b
stitchneno <sakshamgoyal.abhi@proton.me>
2023-10-26 11:33:51 +0530

    changes in script

--- qcacld-3.0.sh
+++ qcacld-3.0.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 #
-# Import or update qcacld-3.0, qca-wifi-host-cmn, fw-api and audio-kernel.
+# Import or update qcacld-3.0, qca-wifi-host-cmn, fw-api, and audio-kernel.
 #
+
+import_or_update() {
+    local prefix=$1
+    local remote_url=$2
+    local branch=$3
+    git subtree add --prefix=$prefix $remote_url $branch
+}
 
 read -p "Please input the tag/branch name: " branch
 read -p "What do you want to do (import (i) or update (u)): " option
 
 case $option in
     import | i)
-        git subtree add --prefix=drivers/staging/qcacld-3.0 https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0 $branch
-        git subtree add --prefix=drivers/staging/qca-wifi-host-cmn https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn $branch
-        git subtree add --prefix=drivers/staging/fw-api https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/fw-api $branch
-        git subtree add --prefix=techpack/audio https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel/ $branch
-        echo "Done."
+        import_or_update "drivers/staging/qcacld-3.0" "https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0" $branch
+        import_or_update "drivers/staging/qca-wifi-host-cmn" "https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn" $branch
+        import_or_update "drivers/staging/fw-api" "https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/fw-api" $branch
+        import_or_update "techpack/audio" "https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel/" $branch
+        echo "Import complete."
         ;;
     update | u)
-        git subtree pull --prefix=drivers/staging/qcacld-3.0 https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0 $branch
-        git subtree pull --prefix=drivers/staging/qca-wifi-host-cmn https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn $branch
-        git subtree pull --prefix=drivers/staging/fw-api https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/fw-api $branch
-        git subtree pull --prefix=techpack/audio https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel/ $branch
-        echo "Done."
+        import_or_update "drivers/staging/qcacld-3.0" "https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0" $branch
+        import_or_update "drivers/staging/qca-wifi-host-cmn" "https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn" $branch
+        import_or_update "drivers/staging/fw-api" "https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/fw-api" $branch
+        import_or_update "techpack/audio" "https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel/" $branch
+        echo "Update complete."
         ;;
     *)
-        echo "Invalid character"
+        echo "Invalid option. Please choose 'import (i)' or 'update (u)."
         ;;
 esac
