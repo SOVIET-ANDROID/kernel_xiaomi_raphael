cf3b2d592009dbe8714a890bb25e0f53bb026d24
corn <69038280+3179245878@users.noreply.github.com>
2025-08-28 19:05:47 +0800

    Update overlayfs.h

--- fs/overlayfs/overlayfs.h
+++ fs/overlayfs/overlayfs.h
@@ -322,3 +322,6 @@ int ovl_copy_up_flags(struct dentry *dentry, int flags);
 int ovl_copy_xattr(struct dentry *old, struct dentry *new);
 int ovl_set_attr(struct dentry *upper, struct kstat *stat);
 struct ovl_fh *ovl_encode_fh(struct dentry *lower, bool is_upper);
+
+int ovl_mount_dir(const char *name, struct path *path);
+
