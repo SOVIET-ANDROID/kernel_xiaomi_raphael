99e33677b771fb2b6b64fec5808a3af2424423ea
corn <69038280+3179245878@users.noreply.github.com>
2025-08-28 20:02:47 +0800

    Update ksu.c

--- drivers/staging/kernelsu/kernel/ksu.c
+++ drivers/staging/kernelsu/kernel/ksu.c
@@ -8,7 +8,7 @@
 #include "allowlist.h"
 #include "arch.h"
 #include "core_hook.h"
-#include "klog.h" // IWYU pragma: keep
+#include "klog.h"
 #include "ksu.h"
 #include "throne_tracker.h"
 #include <linux/path.h>
