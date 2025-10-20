f9f2bb76d952985f7e9e3b5c33b2de046b8beeaf
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-10-20 13:57:10 +0800

    fs/task_mmu: add pkeys header
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -20,7 +20,7 @@
 #include <linux/uaccess.h>
 #include <linux/mm_inline.h>
 #include <linux/ctype.h>
-#include <linux/mm_inline.h>
+#include <linux/pkeys.h>
 
 #include <asm/elf.h>
 #include <asm/tlb.h>
